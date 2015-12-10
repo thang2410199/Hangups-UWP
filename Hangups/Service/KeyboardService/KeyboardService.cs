e64e3416286fc61c89b35f6b9aab3afdbd727898
Ngo Quoc Thang <thang2410199@gmail.com>
2015-12-10 23:18:21 +0200

    Add keyboard service for handling keyboard events (shortcut ?)

---
+++ Hangups/Service/KeyboardService/KeyboardService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hangups.Service
+{
+    public class KeyboardService
+    {
+        KeyboardHelper _helper;
+
+        public KeyboardService()
+        {
+            _helper = new KeyboardHelper();
+            _helper.KeyDown = (e) => { KeyDown?.Invoke(e); };
+            _helper.GoBackGestured = () => { AfterBackGesture?.Invoke(); };
+            _helper.GoForwardGestured = () => { AfterForwardGesture?.Invoke(); };
+        }
+
+        public Action<KeyboardEventArgs> KeyDown { get; set; }
+        public Action AfterBackGesture { get; set; }
+        public Action AfterForwardGesture { get; set; }
+    }
+
+}
