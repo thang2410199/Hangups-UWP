4fa2a494c792eff027a6ba41ee1b7e29a997eba6
Ngo Quoc Thang <thang2410199@gmail.com>
2015-12-10 17:13:23 +0200

    forget to apply to MainPage

--- Hangups/ViewModel/MainViewModel.cs
+++ Hangups/ViewModel/MainViewModel.cs
@@ -1,9 +1,30 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System;
+using GalaSoft.MvvmLight;
+using Hangups.View;
+using Windows.UI.Xaml.Navigation;
 
 namespace Hangups.ViewModel
 {
-    public class MainViewModel : ViewModelBase
+    public class MainViewModel : ViewModelBase, INavigable
     {
+        public bool AllowGoBack()
+        {
+            return true;
+        }
 
+        public void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            
+        }
+
+        public void OnNavigatedTo(NavigationEventArgs e)
+        {
+            
+        }
+
+        public void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            
+        }
     }
 }
